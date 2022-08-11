a25f59939ebef14bd14a828819b5280700761fbd
Roshan Jagtap <91279369+RoshanJ1612@users.noreply.github.com>
2022-08-11 23:30:58 +0530

    Time: 104 ms (48.88%), Space: 33.6 MB (94.15%) - LeetHub

--- 448-find-all-numbers-disappeared-in-an-array/448-find-all-numbers-disappeared-in-an-array.cpp
+++ 448-find-all-numbers-disappeared-in-an-array/448-find-all-numbers-disappeared-in-an-array.cpp
@@ -5,10 +5,7 @@ public:
         sort(nums.begin(),nums.end());
         int n = nums.size();
            nums.erase(unique(nums.begin(), nums.end()), nums.end());
-        for(int i = 0;i<nums.size();i++)
-        {
-            cout<<nums[i]<<" ";
-        }
+     
         vector<int>ans;
         for(int i = 1,j = 0; i<=n;i++)
         {

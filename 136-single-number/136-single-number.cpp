cda00f51e90510a863bc4ba30921c58433339443
Roshan Jagtap <91279369+RoshanJ1612@users.noreply.github.com>
2022-08-12 19:38:16 +0530

    Time: 58 ms (11.64%), Space: 20.7 MB (6.62%) - LeetHub

---
+++ 136-single-number/136-single-number.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) 
+    {
+         map<int, int> M;
+        for (int i = 0; i<nums.size(); i++) 
+        {
+            if (M.find(nums[i]) == M.end()) 
+            {
+                M[nums[i]] = 1;
+            }
+            else
+            {
+                M[nums[i]]++;
+            }
+        }
+        int ans = 0;
+           for (auto& it : M)
+           {
+               if(it.second == 1)
+               {
+                   ans = it.first;
+                   break;
+               }
+           }
+        return ans;
+    }
+        
+};

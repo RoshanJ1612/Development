cbeb3efcfbdf5af149eebc0c85d3f014bbf4073d
Roshan Jagtap <91279369+RoshanJ1612@users.noreply.github.com>
2022-08-15 14:20:18 +0530

    Time: 3 ms (63.12%), Space: 6.3 MB (43.41%) - LeetHub

--- 2129-capitalize-the-title/2129-capitalize-the-title.cpp
+++ 2129-capitalize-the-title/2129-capitalize-the-title.cpp
@@ -1,40 +1,7 @@
 class Solution {
 public:
-    
-   /* string twoletter(string temp)
-    {
-       for(int i = 0; i <s.length();i++)
-        {
-            if(s[i] >= 65 && s[i] <= 90)
-            {
-                s[i] = s[i] + 32;
-            }
-        }
-        return s;
-    }   
-    }
-    
-    string threeletter(string temp)
-    {
-        if(s[0] >= 97 && s[0] <= 122)
-        {
-            s[0] = s[0] - 32;
-        }
-        
-        for(int i = 1; i <s.length();i++)
-        {
-            if(s[i] >= 65 && s[i] <= 90)
-            {
-                s[i] = s[i] + 32;
-            }
-        }
-        return s;
-        
-    }*/
-        
     string capitalizeTitle(string title)
     {
-        string temp;
         
         for(int i = 0; i<title.length();i++)
         {

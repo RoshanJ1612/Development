d23fd4a3a2942aafb918dde4816361dcb8b3d85c
Roshan Jagtap <91279369+RoshanJ1612@users.noreply.github.com>
2022-08-26 23:48:39 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (21.44%) - LeetHub

---
+++ 1952-three-divisors/1952-three-divisors.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool isThree(int n)
+    {
+        int c = 1;
+        for(int i = 1; i<=n/2;i++)
+        {
+            if(n % i == 0)
+            {
+            c++;
+            
+            if(c>3)
+            {
+                return false;
+            }
+            }
+        }
+        
+        if(c<3)
+        {
+            return false;
+        }
+        return true;
+    }
+};

96b5607b943831e69b55d7e2740589ee5146987e
Roshan Jagtap <91279369+RoshanJ1612@users.noreply.github.com>
2022-09-03 18:08:07 +0530

    Time: 84 ms (19.59%), Space: 25.8 MB (91.97%) - LeetHub

--- 495-teemo-attacking/495-teemo-attacking.cpp
+++ 495-teemo-attacking/495-teemo-attacking.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int findPoisonedDuration(vector<int>& t, int d) 
     {
-        int total = 0;
+      /*  int total = 0;
         int c = 1;
         for(int i = 0; i< t.size()-1;i++)
         {
@@ -17,7 +17,17 @@ public:
         }
         
         return (c*d + total);
-    }
+    }*/
+         int total = d, prev = t[0];
+        for (int i = 1; i < t.size(); i++) {
+            int curr = t[i];
+            if (prev + d >= curr) //if the duration of poison overlaps with the previous poison duration
+                total += (curr - prev);
+            else
+                total += d;
+            prev = curr;
+        }
+        return total;
                                  
-                                 
+    }            
 };

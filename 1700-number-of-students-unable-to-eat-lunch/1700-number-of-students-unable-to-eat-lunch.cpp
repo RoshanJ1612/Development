bef76e38f3742d8115d2e78a2818888665e24e4c
Roshan Jagtap <91279369+RoshanJ1612@users.noreply.github.com>
2022-09-13 23:01:56 +0530

    Time: 9 ms (18.15%), Space: 8.6 MB (90.76%) - LeetHub

--- 1700-number-of-students-unable-to-eat-lunch/1700-number-of-students-unable-to-eat-lunch.cpp
+++ 1700-number-of-students-unable-to-eat-lunch/1700-number-of-students-unable-to-eat-lunch.cpp
@@ -1,58 +1,31 @@
 class Solution {
 public:
-     int countStudents(vector<int>& students, vector<int>& sandwiches) 
-     {
-        
-       int indexs[] = {0,0};
-        
-       
-        
-        queue<int> students_q, sandwiches_q;
-        
-     
-        
+    bool isPresent(int a,vector<int>& students)
+    {
         for(int i=0;i<students.size();i++)
         {
-            if(students[i]==0)
-                
-                indexs[0]++;
-            else if(students[i]==1)
-               
-                indexs[1]++;
-         
-            students_q.push(students[i]);
-            sandwiches_q.push(sandwiches[i]);
-        }
-        
-      
-        
-        while(indexs[sandwiches_q.front()]>0 && students_q.size()!=0 )
-        {
-   
-            if(sandwiches_q.front()==students_q.front())
+            if(students[i]==a)
             {
-              
-                indexs[students_q.front()]--;
-               
-                sandwiches_q.pop();
-                
-                students_q.pop();
+                return true;
             }
-             
+        }
+        return false;
+    }
+    int countStudents(vector<int>& students, vector<int>& sandwiches) {
+        while(isPresent(sandwiches.front(),students) && sandwiches.size()>0)
+        {
+            if(sandwiches.front() == students.front())
+            {
+                sandwiches.erase(sandwiches.begin());
+                students.erase(students.begin());
+            }
             else
             {
-            
-                int temp = students_q.front();
-                
-                students_q.pop();
-                students_q.push(temp);
-            }    
-           
-            if(sandwiches_q.size()==0)
-                break;
+                int flag = students.front();
+                students.erase(students.begin());
+                students.push_back(flag);
+            }
         }
-        
-       
-        return students_q.size();
-     }
+        return sandwiches.size();
+    }
 }; 
